22404812a1976422b5962274500ead1671345e01
«Pau <pcodorniu@iesebre.com>
2023-09-19 13:24:59 +0200

    he afegit el primer projecte java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Vaig a mostrar per la pantalla el missatge Hello World!!
+        // Shortcut del System.out.println --> sout
+        System.out.println("Hello World!!");
+        System.out.println("Segona linia de text");
+        System.out.print("Hola soc Pau");
+        System.out.println(" i estic estudian DAM");
+
+    }
+
+
+}
